62a745e6ace6d4e9c04cfc2a83ef923dcc2934aa
Josh Clark <jcinma@gmail.com>
2019-07-10 11:21:35 -0400

    Got JSON return working

--- ip-whois.py
+++ ip-whois.py
@@ -1,6 +1,6 @@
 import requests
 import sys
-import lxml.etree as etree
+import pprint
 
 help_text = '''
 ip-whois help:
@@ -22,8 +22,9 @@ ip = sys.argv[1]
 
 head = {'Accept': r'application/json'}
 url = f'http://whois.arin.net/rest/ip/{ip}'
-r = requests.get(url, params=head, verify=False)
+r = requests.get(url, headers=head, verify=False)
 if r.status_code != 200:
     raise Exception(f'Error code in response: {r.status_code}')
 else:
-    print(etree.tostring(etree.fromstring(r.content), pretty_print=True).decode('utf-8'))
+    print(pprint.pprint(r.json()))
+    
